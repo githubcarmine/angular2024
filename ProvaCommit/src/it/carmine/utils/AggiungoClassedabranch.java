bb522c794140f1586d21e08434026d8143d41c1b
carlo <pippo@libero.it>
2024-02-01 14:26:21 +0100

    aggiungo seconda classe in master

--- ProvaCommit/src/it/carmine/utils/AggiungoClassedabranch.java
+++ ProvaCommit/src/it/carmine/utils/AggiungoClassedabranch.java
@@ -5,6 +5,7 @@ public class AggiungoClassedabranch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Ciao Come va aggiungo classe");
+System.out.println("Aggiungo seconda classe");
 	}
 
 }
