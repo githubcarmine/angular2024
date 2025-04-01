185f66c07d8636df9c20ed6597f015b7c284e338
carmine.quitadamo <carmine.quitadmo@infocamere.it>
2025-04-01 10:24:29 +0200

    faccio modifica

--- ProvaCommit/src/it/carmine/utils/AggiungoClassedabranch.java
+++ ProvaCommit/src/it/carmine/utils/AggiungoClassedabranch.java
@@ -18,7 +18,7 @@ System.out.println("Ciao Come va aggiungo classeaaaa");
 System.out.println("Aggiungo seconda classe");
 System.out.println("Aggiungo terza classe classe");
 System.out.println("Aggiungo quarta classe");
-System.out.println("tolgo classe");
+System.out.println("2026");
 removeDuplicate();
 GetDistinct_Objects();
 	}
